062a18912fd055730c56218c43c0f666399f1943
Vinícius <viniborg25@gmail.com>
2023-06-14 14:54:12 -0300

    criação da estrutura básica do banco de dados
    Foram criadas as tabelas Produto, Cliente e Carrinho com seus respectivos campos.

--- server/apicomercio/models.py
+++ server/apicomercio/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.db.models import AutoField, CharField, Model, FloatField, DateField, ForeignKey, CASCADE, BigIntegerField, IntegerField
 
-# Create your models here.
+# Modelos (tabelas) criados para a aplicação do comércio
+
+class Produto(Model):
+		id_produto = AutoField(primary_key=True)
+		nome_produto = CharField(max_length=50)
+		valor_unit_produto = FloatField()
+		marca_produto = CharField(max_length=30)
+
+class Cliente(Model):
+		cpf_cliente = BigIntegerField(primary_key=True)
+		nome_cliente = CharField(max_length=200)
+		data_nasc_cliente = DateField()
+
+class Carrinho(Model):
+		id_carrinho = AutoField(primary_key=True)
+		cpf_cliente = ForeignKey(Cliente, on_delete=CASCADE)
+		id_produto = ForeignKey(Produto, on_delete=CASCADE)
+		qntd_produto = IntegerField()
+		valor_total_carrinho = FloatField(blank=True, null=True) # Para que possa aceitar valores vazios incialmente
+		
